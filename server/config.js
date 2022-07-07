c7a1e73e180716d943bc06173ab70f02cfc00624
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2022-07-06 21:52:29 -0400

    Upgrade to v0.10.8
    Moved and changed log message for 'fetch'
    Move request log earlier
    Add '-l' option to display GET URLs
    Remove unused dependency - feedparser
    Fix package name and description
    Center text on Server Offline popup
    Display 'Go online' link to site when wiki is offline

--- server/config.js
+++ server/config.js
@@ -2,39 +2,42 @@
 const cfg = {
 
 // ------ Server Options
-  // Port the server listens for requests
-  listenPort: 8000,
+	// Port the server listens for requests
+	listenPort: 8000,
 
-  // Subdirectory path in 'public' to hold TiddlyWikis
-  // Do not include '/public/' - just the subdirectory path
-  // ex: 'tw', 'tiddlywiki/mine', 'store'
-  twDir: 'tiddlywiki',
+	// Subdirectory path in 'public' to hold TiddlyWikis
+	// Do not include '/public/' - just the subdirectory path
+	// ex: 'tw', 'tiddlywiki/mine', 'store'
+	twDir: 'tiddlywiki',
+
+	// Display 'get' URL's on console
+	logUrl: false,
 
 // ------ Page Layout
 // Each web page is displayed in a tiddler as an iframe with class="server-view"
 // ex: <iframe class="server-view" src="/home" />
 
-  // A logo for the site
-  logo: '<img src="/assets/logo.png" style="float: left; width: 24px; margin-right: 6px;">',
+	// A logo for the site
+	logo: '<img src="/assets/logo.png" style="float: left; width: 24px; margin-right: 6px;">',
 
-  // Heading on top of page
-  heading: ``,
+	// Heading on top of page
+	heading: ``,
 
-  // Footer on bottom of page
-  footer: `<div class="footer-text"><hr>Images from <a href="https://pixabay.com" target="_blank">Pixabay</a><hr></div>`,
+	// Footer on bottom of page
+	footer: `<div class="footer-text"><hr>Images from <a href="https://pixabay.com" target="_blank">Pixabay</a><hr></div>`,
 
-  // Display help/hints in web pages
-  inlineHelp: true,
+	// Display help/hints in web pages
+	inlineHelp: true,
 
-  // HTML inserted in page <head> tag
-  //  the '/assigntw.js' script links the page to TiddlyWiki $tw functions
-  head: `
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="/favicon.png" />
-  <script src="/assigntw.js"></script>
-  `,
+	// HTML inserted in page <head> tag
+	//  the '/assigntw.js' script links the page to TiddlyWiki $tw functions
+	head: `
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="/favicon.png" />
+	<script src="/assigntw.js"></script>
+	`,
 
 }
 
